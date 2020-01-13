b2f5b7ec0b9928097f3af9208ec15aaec8e8e5cc
Felix Hartmond <felix@othrys.net>
2020-01-13 23:02:12 +0100

    implemented gamercat processor, restructured modify feed flow

--- feedproxy.go
+++ feedproxy.go
@@ -39,7 +39,7 @@ func processFeed(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, processedFeed)
 }
 
-func getModifyFeedHandler(feedURL string, modifyItem func(*gofeed.Item) *feeds.Item) func() (string, error) {
+func getModifyFeedHandler(feedURL string, modifyItem func(*feeds.Item)) func() (string, error) {
 	return func() (string, error) {
 		oldFeed, err := gofeed.NewParser().ParseURL(feedURL)
 		if err != nil {
@@ -56,50 +56,73 @@ func getModifyFeedHandler(feedURL string, modifyItem func(*gofeed.Item) *feeds.I
 		}
 
 		newFeed.Items = make([]*feeds.Item, len(oldFeed.Items))
-		for index, oldItem := range oldFeed.Items {
-			// TODO Parallelize
-			newFeed.Items[index] = modifyItem(oldItem)
+		progressChan := make(chan(int))
+		modifyItemFunc := func(item *feeds.Item) {
+			modifyItem(item)
+			progressChan <- 1
 		}
 
-		feedString, err := newFeed.ToAtom()
+		for index, oldItem := range oldFeed.Items {
+			newItem := convertItem(oldItem)
+			newFeed.Items[index] = newItem
+			go modifyItemFunc(newItem)
+		}
+
+		for i := 0;  i<len(oldFeed.Items); i++ {
+			<-progressChan
+		}
+
+		feedString, err := newFeed.ToRss()
 		if err != nil {
 			return "", nil
 		}
-		fmt.Println(feedString)
 		return feedString, nil
 	}
 }
 
-func processDilbertItem(*gofeed.Item) *feeds.Item {
-	// TODO implement
-	return &feeds.Item{
-		Title: "Title",
-		Id:    "Id",
-		Link:  &feeds.Link{Href: "Link"},
-		// Description: "Dsc",
-		// Author:      &feeds.Author{Name: "Name", Email: "Email"},
-		// Created:     time.Now(),
+func convertItem(oldItem *gofeed.Item) (newItem *feeds.Item) {
+	newItem = &feeds.Item{
+		Title:   oldItem.Title,
+		Content: oldItem.Content,
+		Link:    &feeds.Link{Href: oldItem.Link},
+		Id:      oldItem.GUID,
 	}
+
+	if oldItem.Author != nil {
+		newItem.Author = &feeds.Author{
+			Name: oldItem.Author.Name,
+			Email: oldItem.Author.Name,
+		}
+	}
+
+	if oldItem.PublishedParsed != nil {
+		newItem.Created = *oldItem.PublishedParsed
+	}
+
+	return
 }
 
-func processGamercatItem(*gofeed.Item) *feeds.Item {
-	// TODO implement
-	return &feeds.Item{
-		Title: "",
-		Link:  &feeds.Link{Href: ""},
-	}
+func processDilbertItem(item *feeds.Item) {
+	// TODO request item.Link.Href
+	// TODO parse title and image url
+	// TODO set Title and Content
+	fmt.Println(item.Link.Href)
+}
+
+func processGamercatItem(item *feeds.Item) {
+	item.Content = strings.Replace(item.Content, "-200x150", "", 1)
 }
 
 func getCommitstrip() (string, error) {
-	feed, err := getFeed("http://www.commitstrip.com/en/feed/")
+	feed, err := getUrlAsString("http://www.commitstrip.com/en/feed/")
 	if err != nil {
 		return "", err
 	}
 	return strings.Split(feed, "</rss>")[0] + "</rss>", nil
 }
 
-func getFeed(feedURL string) (string, error) {
-	resp, err := http.Get(feedURL)
+func getUrlAsString(url string) (string, error) {
+	resp, err := http.Get(url)
 	if err != nil {
 		return "", err
 	}
